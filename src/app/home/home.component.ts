323cf4aa51962c22428a179bbb13f2d7aa4e2488
Doug Deloy <ddeloy@itradenetwork.com>
2017-07-25 18:04:08 -0700

    Update seed to add custom component SidenavMenuItem

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SidenavMenuItem } from '@ddeloy/dd-library';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  menuItems = [
+    new SidenavMenuItem('Settings', 'User Management', '', '', [
+      new SidenavMenuItem('', 'Company Settings', '', '', null),
+      new SidenavMenuItem('', 'User Management', '', '', null),
+      new SidenavMenuItem('', 'Shared Settings', '', '', null)
+    ]),
+    new SidenavMenuItem('', 'Users', '', '', null),
+    new SidenavMenuItem('', 'Non-employee access', '', '', null),
+    new SidenavMenuItem('', 'Roles', '', '', null),
+    new SidenavMenuItem('', 'Data Access Groups', '', '', null),
+    new SidenavMenuItem('', 'DD Groups', '', '', null)
+  ];
 
   constructor() { }
 
