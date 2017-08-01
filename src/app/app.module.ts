8fb7f15abcc2bdac1f9281f4d99d0b803fa58a71
Doug Deloy <ddeloy@itradenetwork.com>
2017-07-31 23:09:18 -0700

    Update seed to Library latest

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule} from '@angular/core';
 import { AppComponent } from './app.component';
-import { LibraryModule } from '@ddeloy/dd-library';
+import { LibraryModule, WijmoTableModule } from '@ddeloy/dd-library';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
 import { BasicComponent } from './basic/basic.component';
 import { SecondaryComponent } from './secondary/secondary.component';
 import { OtherComponent } from './other/other.component';
+
 import { WijmoTableComponent } from './wijmo/wijmo.table.component';
 import { DataSvc } from './services/DataSvc';
 import { NestedMenuComponent } from './custom/nested-menu.component';
@@ -30,6 +31,7 @@ import { routing } from './app.routes';
     LibraryModule,
     CommonModule,
     FormsModule,
+    WijmoTableModule,
     routing
   ],
   providers: [DataSvc],
